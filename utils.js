f2ba0ca0040a390c0bd15c7943dddc0cf6cde521
544-eth <elioku20@gmail.com>
2025-01-12 16:58:27 +0100

    INITIAL COOMIT

--- utils.js
+++ utils.js
@@ -1,4 +1,6 @@
-export function drawStatusText(context, input) {
-    context.font = '30px Helvetica'
-    context.fillText('Last input: ' + input.lastKey, 20, 50)
-}
+export function drawStatusText(context, input, player) {
+    context.font = 'bold 22px Verdana'
+    context.fillStyle = 'yellow';
+    context.fillText('Last input: ' + input.lastKey, 20, 50 )
+    context.fillText('Active state: ' + player.currentState.state, 20, 70)
+} 
